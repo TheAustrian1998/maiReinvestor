2982bb7332df7e87fe6f3d0ce2cee972499bf607
TheAustrian1998 <theaustrianeth@gmail.com>
2021-08-18 15:37:21 -0300

    Added "interfaces" folder, new changes

--- contracts/MaiReinvestor.sol
+++ contracts/MaiReinvestor.sol
@@ -3,39 +3,109 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./IUniswapV2Pair.sol";
-import "./IUsdcSwap.sol";
-import "./IMaiStakingRewards.sol";
-import "./IUniswapV2Router02.sol";
+import "./interfaces/IUniswapV2Pair.sol";
+import "./interfaces/IUsdcSwap.sol";
+import "./interfaces/IMaiStakingRewards.sol";
+import "./interfaces/IUniswapV2Router02.sol";
 
 contract MaiReinvestor is Ownable {
+    address public QuickSwapV2Router02Addr;
+    address public UsdcSwapAddr;
+    address public MaiStakingRewardsAddr;
+    address public UsdcAddr;
+    address public MiMaticAddr;
+    address public MaiAddr;
 
-    IUniswapV2Router02 public QuickSwapV2Router02;
-    IUsdcSwap public UsdcSwap;
-    IERC20 public USDC;
-    IERC20 public miMatic;
+    uint256 public pid;
 
-    constructor(){
+    IUniswapV2Router02 public QuickSwapV2Router02 = IUniswapV2Router02(QuickSwapV2Router02Addr);
+    IUsdcSwap public UsdcSwap = IUsdcSwap(UsdcSwapAddr);
+    IMaiStakingRewards public MaiStakingRewards = IMaiStakingRewards(MaiStakingRewardsAddr);
+    IERC20 public Usdc = IERC20(UsdcAddr);
+    IERC20 public MiMatic = IERC20(MiMaticAddr);
+    IERC20 public Mai = IERC20(MaiAddr);
 
+    constructor(
+        uint256 _pid,
+        address _QuickSwapV2Router02Addr,
+        address _UsdcSwapAddr,
+        address _MaiStakingRewardsAddr,
+        address _UsdcAddr,
+        address _miMaticAddr,
+        address _MaiAddr
+    ) {
+        //Set vars
+        pid = _pid; //Pool Id
+        QuickSwapV2Router02Addr = _QuickSwapV2Router02Addr;
+        UsdcSwapAddr = _UsdcSwapAddr;
+        MaiStakingRewardsAddr = _MaiStakingRewardsAddr;
+        UsdcAddr = _UsdcAddr;
+        MiMaticAddr = _miMaticAddr;
+        MaiAddr = _MaiAddr;
+
+        //Submit approvals
+        Usdc.approve(QuickSwapV2Router02Addr, type(uint).max);
+        Usdc.approve(UsdcSwapAddr, type(uint).max);
+        MiMatic.approve(QuickSwapV2Router02Addr, type(uint).max);
+        Mai.approve(QuickSwapV2Router02Addr, type(uint).max);
     }
 
-    function deposit(uint amount) public onlyOwner {
-        //Deposits USDC
-        USDC.transferFrom(msg.sender, address(this), amount);
+    function deposit(uint256 amount) public onlyOwner {
+        //Deposits Usdc
+        Usdc.transferFrom(msg.sender, address(this), amount);
     }
 
-    function withdraw(uint amount) public onlyOwner{
-        //Withdraw USDC
-        USDC.transfer(msg.sender, amount);
+    function withdraw(uint256 amount) public onlyOwner {
+        //Withdraw Usdc
+        Usdc.transfer(msg.sender, amount);
     }
 
-    function reinvest() public {
+    function reinvest() public onlyOwner {
         //Reinvest all tokens in contract
+        uint256 deadline = block.timestamp + 5 minutes;
 
-        //Swap half of USDC to miMatic
-        uint USDCBalance = USDC.balanceOf(address(this));
-        UsdcSwap.swapFrom(USDCBalance);
+        //Check if Mai balance > 0
+        uint256 MaiBalance = Mai.balanceOf(address(this));
+        if (MaiBalance > 0) {
+            //Swap all for Usdc
+            address[] memory path = new address[](2);
+            path[0] = MaiAddr;
+            path[1] = UsdcAddr;
+            uint256[] memory amountsOut = QuickSwapV2Router02.getAmountsOut(
+                MaiBalance,
+                path
+            );
+            uint256 minAmount = amountsOut[1] - ((amountsOut[1] * 1) / 100); // 1% slippage
+            QuickSwapV2Router02.swapExactTokensForTokens(
+                MaiBalance,
+                minAmount,
+                path,
+                address(this),
+                deadline
+            );
+        }
 
-        //Provide liquidity
+        //Check if Usdc balance > 0
+        uint256 UsdcBalance = Usdc.balanceOf(address(this));
+        uint256 MiMaticBalance = MiMatic.balanceOf(address(this));
+        if (UsdcBalance > 0) {
+            //Swap half of Usdc to MiMatic
+            UsdcSwap.swapFrom(UsdcBalance);
+
+            //Provide liquidity
+            (, , uint256 liquidity) = QuickSwapV2Router02.addLiquidity(
+                UsdcAddr,
+                MiMaticAddr,
+                UsdcBalance,
+                MiMaticBalance,
+                UsdcBalance - ((UsdcBalance * 1) / 100),
+                MiMaticBalance - ((MiMaticBalance * 1) / 100),
+                address(this),
+                deadline
+            );
+
+            //Deposit on Stake (Harvest if possible)
+            MaiStakingRewards.deposit(pid, liquidity);
+        }
     }
 }
